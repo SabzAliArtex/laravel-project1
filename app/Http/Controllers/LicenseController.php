593ce611758a997b67db282405ec72a24ac080e1
MohammadFahad8 <fahadrao1718@gmail.com>
2020-12-09 16:07:08 +0500

    ccvt_license_activate_revised

--- app/Http/Controllers/LicenseController.php
+++ app/Http/Controllers/LicenseController.php
@@ -17,7 +17,7 @@ class LicenseController extends Controller
      */
     public function index()
     {
-        $licenses = License::with('sales_person','user','license_type')->where('is_deleted',NULL)->orderByRaw('id DESC')->get();
+        get();
         // echo '<pre>'; print_r($licenses); exit;
         return view('admin.license.licenselist',compact('licenses'));
     }
@@ -46,15 +46,15 @@ class LicenseController extends Controller
         
         $this->validate($request, [
             "license" => "required",
-            "license_type" => "required",
+            
         ],[
             "license.required" => "Please enter Valid License Code",
-            "license_type.required" => "Pleas select license ",
+            
         ]);
 
         $license = License::create([
             'license' => $request['license'],
-            'license_type_id' => $request['license_type'],
+            
             'sales_person_id' => $request['sales_person'],
         ]);
         
@@ -65,52 +65,64 @@ class LicenseController extends Controller
         $user = License::find($id);
         $user->is_deleted = 1;
         $user->save();
-        Session::flash("success", "Deleted successfully");
+        Session::flash("success", "Deleted Successfully");
         return back();
     }
-    public function licenseActivation($user_id,$sales_person_id,$license_key,$license_id){
-        //Demo Variable Values Start
-        $user_id=2;
-        $sales_person_id = 3;
-        $license_key = 'r123sadas21cc33';
-        //Variables end
+    public function licenseActivation($user_id,$license_key,$dev_name,$dev_model,$dev_id){
+      $response = array();
+      $response['message']="";
+      
+
 
         $userPerson = User::where([['id',$user_id]])->first();
         if($userPerson->role == 2){
-        $userPersons_salesPerson = User::where([['role',3],['id',$sales_person_id]])->first();
-        if(!isset($userPersons_salesPerson)){
-            dd('Not sales person or Sales Person Id is missing');
-        }else{
+      
+        
         //$userPerson->role == 2 means that person is of type 'USER'
-        $license_type = Licensetype::where('id','=',$license_id)->first();
+        
         $license = new License();
+       /*Can be used later
         $license->user_id  = $userPerson->id;
-        $license->sales_person_id = $userPersons_salesPerson->id;
-        $license->license_type_id = $license_type->id ;
         $license->license = $license_key;
-        $license->license_duration = $license_type->license_duration ;
         $license->license_expiry = null ;
-        $license->allowed_test = $license_type->allowed_test;
         $license->trial_activated_at = date("Y-m-d H:i:s") ;
         $license->license_activated_at = date("Y-m-d H:i:s") ;
         $license->device_name ='Example Device Name' ;
         $license->device_model ='Example Model Name' ;
-        $license->device_unique_id = 'Example Machine Address';
-        $license->is_active = 1 ;
-        $license->is_deleted = null;
-        $license->created_at = date("Y-m-d H:i:s");
-        $license->updated_at = date("Y-m-d H:i:s");
-        $license->save();
-            dd($license);
-        }
-        }else{
+        $license->device_unique_id = 'Example Machine Address';*/
 
-            //Person is not a User
-
-
+        if(!isset($license_key)){
+            $response['message'] = "License Key Not Found";
+            return json_encode($response);
         }
 
 
-    }
-    
-}
+        if($license->license != $license_key){
+
+            $response['message'] = "License Key isn't Valid";
+            return json_encode($response);
+            
+        }
+        /*else{
+            $license->is_active == 1;
+
+        }*/
+            if(!isset($dev_model) && !isset($dev_name) && !isset($dev_id))
+            {
+                $response['message'] = "Device Credentials are Invalid";
+            return json_encode($response);
+
+            }else{
+                if($license->device_name != $dev_name && $license->device_model != $dev_model && $license->device_unique_id !=$dev_id){
+
+                    $license->device_name =$dev_name ;
+                    $license->device_model =$dev_model ;
+                    $license->device_unique_id = $dev_id;
+                    $license->is_active == 1;
+                }
+               
+
+            }
+        
+                $license->save();
+  }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
 Auth::routes(['verify'=>true]);
 
