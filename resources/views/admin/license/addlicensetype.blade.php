e76d26cc965c5cf8250c32698ca3034dbdc1bd75
MohammadFahad8 <fahadrao1718@gmail.com>
2021-01-05 15:10:00 +0500

    ccvt_multiple_database

--- resources/views/admin/license/addlicensetype.blade.php
+++ resources/views/admin/license/addlicensetype.blade.php
@@ -81,7 +81,7 @@
                             </div>
                             <div class="form-group row">
                                 <label for="Role"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Select Allowed Test') }}</label>
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Select Allowed Test') }}</label>.e
 
 
                                 <div class="col-md-6">
