0d024581fed3b2ac355ebbd081447d6c0b8d1a45
fahadgill <w1.mtalal@outlook.com>
2021-06-04 06:04:39 -0400

    userrole seeder issue

--- database/seeds/UserRolesSeeder.php
+++ database/seeds/UserRolesSeeder.php
@@ -12,28 +12,23 @@ class UserRolesSeeder extends Seeder
      */
     public function run()
     {
-        //
         DB::table('user_roles')->insert([
             'role' => 'Admin',
             'is_active' => 1,
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
-            
-        ],
-        [
+        ]); 
+        DB::table('user_roles')->insert([
             'role' => 'User',
             'is_active' => 1,
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
-            
-        ],
-        [
+        ]); 
+        DB::table('user_roles')->insert([
             'role' => 'Sales Person',
             'is_active' => 1,
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
-
-            
         ]);
     
     }
