01933dec4f9ed16823f7e8e53acd0bdb82dc0a6f
fahadgill <w1.mtalal@outlook.com>
2021-06-14 02:20:11 -0400

    seeder typo in now fixed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //  $this->call(UsersTableSeeder::class);
-        $this->call(EmailLayouts::class);
+        $this->call(EmailLayoutSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(UserRolesSeeder::class);
         $this->call(PaymentSeeder::class);
